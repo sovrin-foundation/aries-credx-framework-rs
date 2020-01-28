a7b1712bd19c27b97a0db37920d98bfb9a3a6722
Michael Lodder <redmike7@gmail.com>
2020-01-28 13:34:17 -0700

    Refactored implementation
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -1,26 +1,100 @@
+use chrono::DateTime;
+
+use std::ops::{Add, Sub};
+
+/// How many bits are used to shift 1 to get to zero centering
+const BITS_IN_ZERO: usize = 254;
+
 /// Represents an abstract encoder used for converting types to cryptographic integers
+/// Cryptographic integers are limited to 256 bits
 pub trait AttributeEncoder {
     /// The type to represent the cryptographic integer
-    type Output;
+    type Output: Add<Output = Self::Output> + Sub<Output = Self::Output> + From<u64>;
+
+    /// Return the highest value for `Output`
+    fn max() -> Self::Output;
+    /// Return what a value that represents zero
+    fn zero_center() -> Self::Output;
+    /// Takes a vector of bytes and returns `Self::Output`
+    fn from_vec(v: Vec<u8>) -> Self::Output;
 
     /// Takes an date string that is formatted according to RFC3339
     /// and converts it to a cryptographic integer. 
     /// `value`: Any type that can be converted into a string slice
-    fn from_rfc3339<'a, A: Into<&'a str>>(value: A) -> Result<Self::Output, String>;
+    fn encode_from_rfc3339_as_unixtimestamp<'a, A: Into<&'a str>>(value: A) -> Result<Self::Output, String> {
+        let dt = DateTime::parse_from_rfc3339(value.into()).map_err(|e| format!("{:?}", e))?;
+        Ok(Self::zero_center() + Self::Output::from(dt.timestamp() as u64))
+    }
+
+    /// Takes an date string that is formatted according to RFC3339
+    /// and converts it to a cryptographic integer. 
+    /// `value`: Any type that can be converted into a string slice
+    fn encode_from_rfc3339_as_dayssince1900<'a, A: Into<&'a str>>(value: A) -> Result<Self::Output, String> {
+        let dt = DateTime::parse_from_rfc3339(value.into()).map_err(|e| format!("{:?}", e))?;
+        let base = DateTime::parse_from_rfc3339("1900-01-01T00:00:00.000+00:00").map_err(|e| format!("{:?}", e))?;
+        Ok(Self::zero_center() + Self::Output::from((dt - base).num_days() as u64))
+    }
+
     /// Takes a 64-bit floating point number and converts it into
     /// a cryptographic integer
     /// `value`: Any type that can be converted into a f64
-    fn from_f64<A: Into<f64>>(value: A) -> Result<Self::Output, String>;
+    fn encode_from_f64<A: Into<f64>>(v: A) -> Result<Self::Output, String> {
+        use std::num::FpCategory::*;
+        use num_bigint::Sign::*;
+
+        let value = v.into();
+
+        Ok(
+            match value.classify() {
+                Nan => Self::Output::from(1),
+                Subnormal => Self::Output::from(2),
+                Zero => Self::zero_center(),
+                Infinite => {
+                    if value.is_sign_positive() {
+                        Self::max() - Self::Output::from(9)
+                    } else {
+                        Self::Output::from(8)
+                    }
+                },
+                Normal => {
+                    let mut b = bigdecimal::BigDecimal::from(value);
+
+                    for _ in 0..BITS_IN_ZERO {
+                        b = b.double();
+                    }
+                    let (bi, _) = b.into_bigint_and_exponent();
+                    let (sign, bytes) = bi.to_bytes_be();
+                    let f = Self::from_vec(bytes);
+                    match sign {
+                        NoSign => Self::zero_center(),
+                        Plus => f,
+                        Minus => Self::zero_center() - f
+                    }
+                }
+            }
+        )
+    }
+
     /// Takes a signed number and converts it into
     /// a cryptographic integer
     /// `value`: Any type that can be converted into a isize
-    fn from_isize<A: Into<isize>>(value: A) -> Result<Self::Output, String>;
+    fn encode_from_isize<A: Into<isize>>(value: A) -> Result<Self::Output, String> {
+        let value = value.into();
+        if value < 0 {
+            Ok(Self::zero_center() - Self::Output::from(-value as u64))
+        } else {
+            Ok(Self::zero_center() + Self::Output::from(value as u64))
+        }
+    }
 
     /// Takes an unsigned number and converts it into
     /// a cryptographic integer
     /// `value`: Any type that can be converted into a usize
-    fn from_usize<A: Into<usize>>(value: A) -> Result<Self::Output, String>;
+    fn encode_from_usize<A: Into<usize>>(value: A) -> Result<Self::Output, String> {
+        Ok(Self::zero_center() + Self::Output::from(value.into() as u64))
+    }
 }
+
 
 /// Provides an encoder to BLS12-381 FieldElements
 #[cfg(feature = "bls381")]
--- src/encoding/rsa_native.rs
+++ src/encoding/rsa_native.rs
@@ -1,113 +1,87 @@
-use super::AttributeEncoder;
+use super::{AttributeEncoder, BITS_IN_ZERO};
 
-use chrono::DateTime;
 use openssl::{
     bn::{BigNum, BigNumRef}
 };
 
-/// How many bits are used to shift 1 to get to zero centering
-pub const BITS_IN_ZERO: i32 = 254;
+use std::{cmp::{Eq, PartialEq}, ops::{Add, Sub}};
 
-/// 2^BITS_IN_ZERO. Zero centers numbers used as ECC field elements
-/// Should only be used where comparisons are used
-fn zero() -> BigNum {
-    let mut bn = BigNum::from_u32(1).unwrap();
-    bn.set_bit(BITS_IN_ZERO).unwrap();
-    bn
-} 
+/// A simple wrapper class for converting attributes to cryptographic integers
+/// represented in OpenSSL's BigNum library
+#[derive(Debug)]
+pub struct BigNumber(pub BigNum);
 
-fn max() -> BigNum {
-    BigNum::from_hex_str("FFFFFFFF_FFFFFFFF_FFFFFFFF_FFFFFFFF_FFFFFFFF_FFFFFFFF_FFFFFFFFF_FFFFFFFF").unwrap()
-}
+impl Add for BigNumber {
+    type Output = Self;
 
-/// Represents an integer than can be cryptographically signed
-pub struct OpenSslAttributeEncoder;
-
-impl AttributeEncoder for OpenSslAttributeEncoder {
-    type Output = BigNum;
-
-    /// Takes an date string that is formatted according to RFC3339
-    /// and converts it to a cryptographic integer. 
-    /// `value`: Any type that can be converted into a string slice
-    fn from_rfc3339<'a, A: Into<&'a str>>(value: A) -> Result<Self::Output, String> {
-        let dt = DateTime::parse_from_rfc3339(value.into()).map_err(|e| format!("{:?}", e))?;
-        Self::from_usize(dt.timestamp() as usize)
-    }
-
-    /// Takes a 64-bit floating point number and converts it into
-    /// a cryptographic integer
-    /// `value`: Any type that can be converted into a f64
-    fn from_f64<A: Into<f64>>(value: A) -> Result<Self::Output, String> {
-        use std::num::FpCategory::*;
-        use num_bigint::Sign::*;
-
-        let value = value.into();
-
-        Ok(
-        match value.classify() {
-            Nan => BigNum::from_u32(1).unwrap(),
-            Subnormal => BigNum::from_u32(2).unwrap(),
-            Zero => zero(),
-            Infinite => {
-                if value.is_sign_positive() {
-                    sub(&max(), &BigNum::from_u32(9).unwrap())
-                } else {
-                    BigNum::from_u32(8).unwrap()
-                }
-            },
-            Normal => {
-                let mut b = bigdecimal::BigDecimal::from(value);
-
-                for _ in 0..BITS_IN_ZERO {
-                    b = b.double();
-                }
-                let (bi, _) = b.into_bigint_and_exponent();
-                let (sign, bytes) = bi.to_bytes_be();
-                let f = BigNum::from_slice(bytes.as_slice()).unwrap();
-                match sign {
-                    NoSign => zero(),
-                    Plus => f,
-                    Minus => sub(&zero(), &f)
-                }
-            }
-        })
-    }
-
-    /// Takes a signed number and converts it into
-    /// a cryptographic integer
-    /// `value`: Any type that can be converted into a isize
-    fn from_isize<A: Into<isize>>(value: A) -> Result<Self::Output, String> {
-        let value = value.into();
-        if value < 0 {
-            let value = -value as u64;
-            Ok(sub(&zero(), &u64_to_bignum(value)))
-        } else {
-            Self::from_usize(value as usize)
-        }
-    }
-
-    /// Takes an unsigned number and converts it into
-    /// a cryptographic integer
-    /// `value`: Any type that can be converted into a usize
-    fn from_usize<A: Into<usize>>(value: A) -> Result<Self::Output, String> {
-        Ok(add(&zero(), &u64_to_bignum(value.into() as u64)))
+    fn add(self, rhs: Self::Output) -> Self::Output {
+        let mut bn = BigNum::new().unwrap();
+        BigNumRef::checked_add(&mut bn, &self.0, &rhs.0).unwrap();
+        BigNumber(bn)
     }
 }
 
-fn u64_to_bignum(v: u64) -> BigNum {
-    BigNum::from_dec_str(&format!("{}", v)).unwrap()
+impl<'a, 'b> Add<&'b BigNumber> for &'a BigNumber {
+    type Output = BigNumber;
+
+    fn add(self, rhs: &'b BigNumber) -> BigNumber {
+        let mut bn = BigNum::new().unwrap();
+        BigNumRef::checked_add(&mut bn, &self.0, &rhs.0).unwrap();
+        BigNumber(bn)
+    }
 }
 
-fn add(a: &BigNumRef, b: &BigNumRef) -> BigNum {
-    let mut bn = BigNum::new().unwrap();
-    BigNumRef::checked_add(&mut bn, &a, &b).unwrap();
-    bn
+impl Sub for BigNumber {
+    type Output = Self;
+
+    fn sub(self, rhs: Self::Output) -> Self::Output {
+        let mut bn = BigNum::new().unwrap();
+        BigNumRef::checked_sub(&mut bn, &self.0, &rhs.0).unwrap();
+        BigNumber(bn)
+    }
 }
 
-fn sub(a: &BigNumRef, b: &BigNumRef) -> BigNum {
-    let mut bn = BigNum::new().unwrap();
-    BigNumRef::checked_sub(&mut bn, &a, &b).unwrap();
-    bn
+impl<'a, 'b> Sub<&'b BigNumber> for &'a BigNumber {
+    type Output = BigNumber;
+
+    fn sub(self, rhs: &'b Self::Output) -> Self::Output {
+        let mut bn = BigNum::new().unwrap();
+        BigNumRef::checked_sub(&mut bn, &self.0, &rhs.0).unwrap();
+        BigNumber(bn)
+    }
+}
+
+impl From<u64> for BigNumber {
+    fn from(v: u64) -> Self {
+        BigNumber(BigNum::from_slice(&v.to_be_bytes()[..]).unwrap())
+    }
+}
+
+impl PartialEq for BigNumber {
+    fn eq(&self, other: &BigNumber) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Eq for BigNumber{}
+
+impl AttributeEncoder for BigNumber {
+    type Output = BigNumber;
+
+    fn max() -> Self::Output {
+        let bytes = vec![0xFF; 32];
+        Self(BigNum::from_slice(bytes.as_slice()).unwrap())
+    }
+
+    fn zero_center() -> Self::Output {
+        let mut bn = BigNum::from_u32(1).unwrap();
+        bn.set_bit(BITS_IN_ZERO as i32).unwrap();
+        Self(bn)
+    }
+
+    fn from_vec(bytes: Vec<u8>) -> Self::Output {
+        Self(BigNum::from_slice(bytes.as_slice()).unwrap()) 
+    }
 }
 
 #[cfg(test)]
@@ -116,69 +90,69 @@ mod tests {
 
     #[test]
     fn rfc3339_string_convert() {
-        let res = OpenSslAttributeEncoder::from_rfc3339("2018-01-26T18:30:09.453+00:00");
+        let res = BigNumber::encode_from_rfc3339_as_unixtimestamp("2018-01-26T18:30:09.453+00:00");
         assert!(res.is_ok());
-        assert_eq!(add(&BigNum::from_u32(1_516_991_409u32).unwrap(), &zero()), res.unwrap());
+        assert_eq!(BigNumber::from(1_516_991_409) + BigNumber::zero_center(), res.unwrap());
 
-        let res = OpenSslAttributeEncoder::from_rfc3339("2020-01-26T00:30:09.000+18:00");
+        let res = BigNumber::encode_from_rfc3339_as_unixtimestamp("2020-01-26T00:30:09.000+18:00");
         assert!(res.is_ok());
-        assert_eq!(add(&BigNum::from_u32(1_579_933_809u32).unwrap(), &zero()), res.unwrap());
+        assert_eq!(BigNumber::from(1_579_933_809) + BigNumber::zero_center(), res.unwrap());
 
-        let res = OpenSslAttributeEncoder::from_rfc3339("1970-01-01T00:00:00.000+00:00");
+        let res = BigNumber::encode_from_rfc3339_as_unixtimestamp("1970-01-01T00:00:00.000+00:00");
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), zero());
+        assert_eq!(res.unwrap(), BigNumber::zero_center());
 
-        let res = OpenSslAttributeEncoder::from_rfc3339("1900");
+        let res = BigNumber::encode_from_rfc3339_as_unixtimestamp("1900");
         assert!(res.is_err());
     }
 
     #[test]
     fn decimal_test() {
-        let res1 = OpenSslAttributeEncoder::from_f64(1.33f32);
+        let res1 = BigNumber::encode_from_f64(1.33f32);
         assert!(res1.is_ok());
-        let res2 = OpenSslAttributeEncoder::from_f64(-1.33f32);
+        let res2 = BigNumber::encode_from_f64(-1.33f32);
         assert!(res2.is_ok());
-        assert_eq!(zero(), add(&res1.unwrap(), &res2.unwrap()));
+        assert_eq!(BigNumber::zero_center(), res1.unwrap() + res2.unwrap());
 
-        let res1 = OpenSslAttributeEncoder::from_f64(std::f64::MAX);
+        let res1 = BigNumber::encode_from_f64(std::f64::MAX);
         assert!(res1.is_ok());
         let res2 = res1.unwrap();
-        assert_eq!(sub(&res2, &res2), BigNum::new().unwrap());
+        assert_eq!((&res2 - &res2).0, BigNum::new().unwrap());
 
-        let res3 = OpenSslAttributeEncoder::from_f64(std::f64::MIN);
+        let res3 = BigNumber::encode_from_f64(std::f64::MIN);
         assert!(res3.is_ok());
-        assert_eq!(zero(), add(&res3.unwrap(), &res2));
+        assert_eq!(BigNumber::zero_center(), &res3.unwrap() + &res2);
 
-        let res1 = OpenSslAttributeEncoder::from_f64(std::f64::NEG_INFINITY);
+        let res1 = BigNumber::encode_from_f64(std::f64::NEG_INFINITY);
         assert!(res1.is_ok());
-        assert_eq!(BigNum::from_u32(8).unwrap(), res1.unwrap());
+        assert_eq!(BigNum::from_u32(8).unwrap(), res1.unwrap().0);
 
-        let pos_inf = sub(&max(), &BigNum::from_u32(9).unwrap());
-        let res1 = OpenSslAttributeEncoder::from_f64(std::f64::INFINITY);
+        let pos_inf = BigNumber::max() - BigNumber::from(9);
+        let res1 = BigNumber::encode_from_f64(std::f64::INFINITY);
         assert!(res1.is_ok());
         assert_eq!(pos_inf, res1.unwrap());
 
-        let res1 = OpenSslAttributeEncoder::from_f64(std::f64::NAN);
+        let res1 = BigNumber::encode_from_f64(std::f64::NAN);
         assert!(res1.is_ok());
-        assert_eq!(BigNum::from_u32(1).unwrap(), res1.unwrap());
+        assert_eq!(BigNum::from_u32(1).unwrap(), res1.unwrap().0);
     }
 
     #[test]
     fn size_test() {
-        let res = OpenSslAttributeEncoder::from_isize(0isize);
+        let res = BigNumber::encode_from_isize(0isize);
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), zero());
-        let res = OpenSslAttributeEncoder::from_isize(1isize);
+        assert_eq!(res.unwrap(), BigNumber::zero_center());
+        let res = BigNumber::encode_from_isize(1isize);
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), add(&zero(), &BigNum::from_u32(1).unwrap()));
-        let res = OpenSslAttributeEncoder::from_isize(-1isize);
+        assert_eq!(res.unwrap(), BigNumber::zero_center() + BigNumber::from(1));
+        let res = BigNumber::encode_from_isize(-1isize);
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), sub(&zero(), &BigNum::from_u32(1).unwrap()));
-        let res = OpenSslAttributeEncoder::from_isize(std::isize::MAX);
+        assert_eq!(res.unwrap(), BigNumber::zero_center() - BigNumber::from(1));
+        let res = BigNumber::encode_from_isize(std::isize::MAX);
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), add(&zero(), &u64_to_bignum(std::isize::MAX as u64)));
-        let res = OpenSslAttributeEncoder::from_usize(std::usize::MAX);
+        assert_eq!(res.unwrap(), BigNumber::zero_center() + BigNumber::from(std::isize::MAX as u64));
+        let res = BigNumber::encode_from_usize(std::usize::MAX);
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), add(&zero(), &u64_to_bignum(std::usize::MAX as u64)));
+        assert_eq!(res.unwrap(), BigNumber::zero_center() + BigNumber::from(std::u64::MAX));
     }
 }
